fd7cffd09ae4e8ae38620f667bc4cef82d85cc8f
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-17 15:25:23 +0100

    kill pypy3 process

--- run-test.sh
+++ run-test.sh
@@ -7,7 +7,7 @@ function test_case {
   ./app.py &>> /tmp/flasklog.log &
   sleep 1
   for i in {1..1000}; do curl -s http://localhost:5000 >/dev/null && curl -s http://localhost:5000/static/tux.png >/dev/null;done
-  kill $(ps aux | egrep '.*python.*app.py$' | awk '{ print $2 }')
+  kill $(ps aux | egrep '.*py.*app.py$' | awk '{ print $2 }')
 }
 
 for i in $(seq 1 3); do
