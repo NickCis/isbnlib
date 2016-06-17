5615b6fcfc56a12abdf39b953611f712d6a3ceb1
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-17 15:00:39 +0100

    silent curl

--- run-test.sh
+++ run-test.sh
@@ -6,10 +6,8 @@ rm -f /tmp/flasklog.log
 function test_case {
   ./app.py &>> /tmp/flasklog.log &
   sleep 1
-  for i in {1..1000}; do curl http://localhost:5000 >/dev/null && curl http://localhost:5000/static/tux.png >/dev/null;done
-  
+  for i in {1..1000}; do curl -s http://localhost:5000 >/dev/null && curl -s http://localhost:5000/static/tux.png >/dev/null;done
   kill $(ps aux | egrep '.*python.*app.py$' | awk '{ print $2 }')
-  
 }
 
 for i in $(seq 1 5); do
