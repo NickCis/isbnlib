6031ddca68f1450879928d83655e05ec54648f1c
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-20 10:40:16 +0100

    skip non-Flask errors

--- run-test.sh
+++ run-test.sh
@@ -8,10 +8,10 @@ function test_case {
   sleep 5
   for i in {1..100}
   do 
-    curl -s http://localhost:5000 >/dev/null
-    curl -s http://localhost:5000/static/tux.png >/dev/null
-    curl -s http://localhost:5000/static/tux1.png >/dev/null
-    curl -s http://localhost:5000/static/tux2.png >/dev/null
+    curl -s http://localhost:5000 &>/dev/null
+    curl -s http://localhost:5000/static/tux.png &>/dev/null
+    curl -s http://localhost:5000/static/tux1.png &>/dev/null
+    curl -s http://localhost:5000/static/tux2.png &>/dev/null
     touch app.py
   done
   kill $(ps aux | egrep '.*py.*app.py$' | awk '{ print $2 }')
@@ -19,6 +19,7 @@ function test_case {
 
 for i in $(seq 1 3); do
   echo "Run number ${i}"
+  # skip non-Flask errors
   test_case 2>/dev/null
 done
 
