6dae688d801f7147ebfa7ac1ef2afa3074b201af
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-20 11:16:07 +0100

    skip non-Flask errors

--- run-test.sh
+++ run-test.sh
@@ -8,10 +8,10 @@ function test_case {
   sleep 5
   for i in {1..100}
   do 
-    curl -s http://localhost:5000 &>/dev/null
-    curl -s http://localhost:5000/static/tux.png &>/dev/null
-    curl -s http://localhost:5000/static/tux1.png &>/dev/null
-    curl -s http://localhost:5000/static/tux2.png &>/dev/null
+    curl -s http://localhost:5000 >/dev/null
+    curl -s http://localhost:5000/static/tux.png >/dev/null
+    curl -s http://localhost:5000/static/tux1.png >/dev/null
+    curl -s http://localhost:5000/static/tux2.png >/dev/null
     touch app.py
   done
   kill $(ps aux | egrep '.*py.*app.py$' | awk '{ print $2 }')
@@ -20,8 +20,8 @@ function test_case {
 for i in $(seq 1 3); do
   echo "Run number ${i}"
   # skip non-Flask errors
-  test_case 2>/dev/null || true
-  [[ ! $? = 0 ]] && echo "Non-Flask error $?"
+  test_case || true
+  #[[ ! $? = 0 ]] && echo "Non-Flask error $?"
 done
 
 # check log to see if there are Flask errors 
